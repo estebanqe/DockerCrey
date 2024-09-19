ee4bae871030823e51824cdabacd1db55ab55401
estebanqe <157732736+estebanqe@users.noreply.github.com>
2024-09-18 21:30:30 -0500

    api
    api

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 source venv/bin/activate
 pip install --upgrate pip
 pip install -r requirements.txt
-reflex init
-reflex export --frontend-only
 rm -rf public
+reflex init
+API_URL=https://dockercrey.up.railway.app/ reflex export --frontend-only
 unzip frontend.zip -d public
 rm -f frontend.zip
 deactivated
